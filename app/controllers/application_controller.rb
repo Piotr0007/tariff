4b50bdf814c0ebc730c049a52ad9b22f377b5e1c
Piotr0007 <piozajac@onet.pl>
2018-06-16 20:09:48 +0200

    Create new user and new view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-
-  
+  def create
+  	@user = User.new(params.require[:user].permit(:name, :lastname, :email, :phone, :password))
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  def index
+  end  
   def new
   	@user =User.new
 
@@ -15,15 +16,15 @@ class UsersController < ApplicationController
  # 	end      
  # end 
   def create
-    @user = User.new(params.require[:user].permit(:name, :lastname, :email, :phone, :password))
+     @user = User.new(params.require(:user).permit(:name, :lastname, :email, :phone, :password, :password_confirmation))
+   # @user = User.new(params.require[:user].permit(:name, :lastname, :email, :phone, :password))
     @user.save
     redirect_to @user
   end  
   def show
     @user = User.find(params[:id])
   end
-  def devise
-  end
+
 
 
 end
